3dd26e1c3668d0735137f8be0d465f7ab81980af
adnptrpw <andini.putri02@ui.ac.id>
2023-03-25 22:37:11 +0700

    remove Lottie and create splashScreen

---
+++ monti/Controller/SplashView.swift
@@ -0,0 +1,55 @@
+//
+//  SplashView.swift
+//  monti
+//
+//  Created by Andini Putri Pramudya Wardani on 24/03/23.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    
+    @State var splashScreen = ""
+    @State var isActive: Bool = false
+    
+    var body: some View {
+        ZStack{
+            if self.isActive{
+                ContentView()
+            } else {
+                VStack(alignment: .center){
+                    Image(splashScreen)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .edgesIgnoringSafeArea(.all)
+                        .onAppear(perform: showMonti)
+                }
+            }
+        } .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 4.02) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+    
+    func showMonti(){
+            
+      var index = 0
+        _ = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true){
+            
+            (Timer) in
+            splashScreen = "main\(index)"
+            index += 1
+            
+            }
+        }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
---
+++ monti/Views/YourPictureView.swift
@@ -0,0 +1,20 @@
+//
+//  YourPictureView.swift
+//  monti
+//
+//  Created by Andini Putri Pramudya Wardani on 24/03/23.
+//
+
+import SwiftUI
+
+struct YourPictureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct YourPictureView_Previews: PreviewProvider {
+    static var previews: some View {
+        YourPictureView()
+    }
+}
